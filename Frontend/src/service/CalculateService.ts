309942953a805549b48a741c4a935d5f94514250
phattanothai <zeref38@gmail,com>
2022-08-08 19:32:04 +0700

    update grpc

--- Frontend/src/service/CalculateService.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  base64_encode(str: any) {
-    return Buffer.from(str).toString("base64");
-  },
-  base64_decode(str: any) {
-    return Buffer.from(str, "base64").toString();
-  },
-};
--- Frontend/src/store/user/action-types.ts
+++
@@ -1,5 +0,0 @@
-export enum AuthActionTypes {
-  GetTaskItems = "GET_Task_ITEMS",
-  SetCreateModal = "SET_CREATE_MODAL",
-  SetEditModal = "SET_EDIT_MODAL",
-}
